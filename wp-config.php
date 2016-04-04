302331aa9277645b11d63c9adebeecdf74c318a1
Stergios Michopoulos <stergios.michopoulos@gmail.com>
2016-04-04 21:47:01 +0200

    plugin

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,7 @@ $table_prefix  = 'wp_';
  * 
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */ 
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* Det var allt, sluta redigera här! Blogga på. */
 
